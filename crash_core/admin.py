5ac1ca5ca29412699bc5ca3f520f85eef6c16637
pavel.makhnev <p_makhnev@ural-press.ru>
2022-07-21 23:32:24 +0500

    add model for Frame

--- crash_core/admin.py
+++ crash_core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Frame
 
 # Register your models here.
+
+
+@admin.register(Frame)
+class FrameAdmin(admin.ModelAdmin):
+    pass
--- crash_core/models.py
+++ crash_core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Frame(models.Model):
+    file_name = models.CharField(max_length=20, blank=False, unique=False)
+    file_bin = models.BinaryField(editable=True)
+    update_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return "{1} - {0}".format(self.file_name, self.update_date)
