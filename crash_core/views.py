eb761d767269746a26300457172c92a68c3caca3
pavel.makhnev <p_makhnev@ural-press.ru>
2022-07-21 23:33:12 +0500

    add first view

--- crash_core/views.py
+++ crash_core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Frame
 
 # Create your views here.
+
+
+def main_page(request):
+    f = Frame.objects.latest('update_date')
+
+    return render(request, "main_page.html", context={"f": f})
