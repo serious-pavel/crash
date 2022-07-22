bcde606e8b487fae5f72aec517dccd7968919805
pavel.makhnev <p_makhnev@ural-press.ru>
2022-07-22 18:04:55 +0500

    fix model to reconnect when problems

--- crash_core/models.py
+++ crash_core/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from base64 import b64encode
+from django.db.utils import OperationalError, InterfaceError
+from psycopg2 import OperationalError as oe
+import time
+
+from django.db import connection
+
 
 # Create your models here.
 
@@ -18,3 +24,28 @@ class Frame(models.Model):
 
     def __str__(self):
         return "{1} - {0}".format(self.file_name, self.update_date)
+
+    def upload(self, t=0):
+        if t > 5:
+            raise Exception('Error saving file to the database')
+        else:
+            try:
+                self.save()
+            except OperationalError as ex:
+                print(f'{ex}')
+                time.sleep(5)
+                self.upload(t+1)
+            except InterfaceError as ex:
+                print(f'{ex}. Trying to reconnect')
+                connection.connection.close()
+                connection.connection = None
+                time.sleep(5)
+                self.upload(t+1)
+            except oe:
+                pass
+            except KeyboardInterrupt:
+                print('Stopped')
+                pass
+
+
+
