45cb22cb6c34587308e437e8e8c8dcc524195ed8
pavel.makhnev <p_makhnev@ural-press.ru>
2022-07-22 12:25:41 +0500

    add picture on main_page

--- crash_core/models.py
+++ crash_core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from base64 import b64encode
 
 # Create your models here.
 
@@ -8,6 +9,12 @@ class Frame(models.Model):
     file_name = models.CharField(max_length=20, blank=False, unique=False)
     file_bin = models.BinaryField(editable=True)
     update_date = models.DateTimeField(default=timezone.now)
+
+    get_latest_by = "-update_date"
+
+    @property
+    def img(self):
+        return b64encode(self.file_bin).decode()
 
     def __str__(self):
         return "{1} - {0}".format(self.file_name, self.update_date)
